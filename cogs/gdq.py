22acf6bce6b0875b51470259afcb86ae879b6768
PSDevChris <christopher.schmid.1993@gmail.com>
2021-05-31 16:17:19 +0200

    Refactored GDQGame Cog Less iteration, shorter code

--- cogs/gdq.py
+++ cogs/gdq.py
@@ -18,35 +18,22 @@ class GDQ(commands.Cog):
     ### Get the GDQ Schedule and show which game is running ###
     @commands.command(name="gdqgame", aliases=["GDQgame", "GDQGame"], brief="Gibt das aktuelle GDQ Game, Runner und Kategorie aus")
     async def _gdqgame(self, ctx):
-        ListOfRuns = []
-        ListOfTime = []
         GDQ_URL = "https://gamesdonequick.com/schedule"
         GDQ_REQ = requests.get(GDQ_URL)
         GDQ_DATAFRAME = pd.read_html(GDQ_REQ.text)
         for Entry in GDQ_DATAFRAME:
-            ListOfRuns.append(Entry["Run"])
-            ListOfTime.append(Entry["Time & Length"])
-        mydf = pd.DataFrame()
-        for ListElement in ListOfRuns:
-            mydf["Runs"] = ListElement
-        for ListElement in ListOfTime:
-            mydf["Time and Length"] = ListElement
-        for i in range(len(mydf["Time and Length"])):
-            if i % 2 == 0:
-                GameTime = parse(mydf["Time and Length"].iloc[i])
-                GameDuration = datetime.strptime(
-                    mydf["Time and Length"].iloc[i+1], "%H:%M:%S")
-                GameDelta = timedelta(
-                    hours=GameDuration.hour, minutes=GameDuration.minute, seconds=GameDuration.second)
+            for index in range(0, len(Entry["Run"]), 2):
+                runEntry = Entry["Run"]
+                timeEntry = Entry["Time & Length"]
+                GameTime = parse(timeEntry[index])
+                GameDuration = datetime.strptime(timeEntry[index+1], "%H:%M:%S")
+                GameDelta = timedelta(hours=GameDuration.hour, minutes=GameDuration.minute, seconds=GameDuration.second)
                 GameTimeStamp = datetime.timestamp(GameTime + GameDelta)
-                if datetime.timestamp(datetime.now()) < GameTimeStamp:
-                    await ctx.send(f"Bei GDQ läuft gerade {mydf['Runs'].iloc[i]} {mydf['Runs'].iloc[i+1]}!")
+                if datetime.timestamp(datetime.now()) < GameTimeStamp and datetime.now().date() == GameTime.date():
+                    await ctx.send(f"Bei GDQ läuft gerade {runEntry[index]} {runEntry[index+1]}!")
                     break
             else:
-                if i == len(mydf["Time and Length"])-1:
-                    await ctx.send("GDQ ist vorbei, beehre uns bald wieder.")
-                else:
-                    continue
+                await ctx.send("GDQ ist vorbei oder noch nicht angefangen, beehre uns bald wieder.")
 
 def setup(bot):
     bot.add_cog(GDQ(bot))
