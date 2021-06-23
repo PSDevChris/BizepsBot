929edb7801f60bdeabf7451e8f4a93e2d604d147
PSDevChris <christopher.schmid.1993@gmail.com>
2021-06-23 09:34:52 +0200

    Changed check to if clause to hide uwu command

--- Main.py
+++ Main.py
@@ -239,16 +239,16 @@ class Fun(commands.Cog, name="Schabernack"):
             logging.info(
                 f"{ctx.author} hat ein Meme erfolgreich eingesammelt.")
 
-    @commands.command()
     @commands.Cog.listener("on_message")
     @commands.check(_is_nouwuchannel)
-    async def _uwumsg(self, message):
-        if message.author == bot.user:
-            return
-        if random.randint(0, 50) == 1:
-            LastMessageContent = message.content
-            flags = uwuify.SMILEY | uwuify.YU
-            await message.channel.send(f"{uwuify.uwu(LastMessageContent, flags=flags)} UwU")
+    async def _uwumsg(self, ctx, message):
+        if ctx.message.channel.category_id != 539547423782207488 and ctx.message.channel.id not in [539549544585756693, 539546796939149334]:
+            if message.author == bot.user:
+                return
+            if random.randint(0, 50) == 1:
+                LastMessageContent = message.content
+                flags = uwuify.SMILEY | uwuify.YU
+                await message.channel.send(f"{uwuify.uwu(LastMessageContent, flags=flags)} UwU")
 
     @commands.command(name="uwu", aliases=["UwU", "Uwu", "uWu", "uWU"], brief="Weebt die Message UwU")
     @commands.cooldown(1, 30, commands.BucketType.user)
