12089d17793e138b2d2c80af897d575c0ecf0dd0
PSDevChris <christopher.schmid.1993@gmail.com>
2021-07-08 17:27:12 +0200

    Added a lot of documentation Added new tasks for hans

--- Main.py
+++ Main.py
@@ -283,7 +283,9 @@ class Fun(commands.Cog, name="Schabernack"):
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def _hansworks(self, ctx):
         HansTasks = ["schneiden", "rendern",
-                     "hochladen", "arbeiten", "eine rauchen", "irgendwelche npm Abhängigkeiten fixen", "Code aus 1986 debuggen"]
+                     "hochladen", "arbeiten", "eine rauchen",
+                     "irgendwelche npm Abhängigkeiten fixen",
+                     "Code aus 1986 debuggen", "Mövius anbeten"]
         HansTask = random.choice(HansTasks)
         await ctx.send(f"Hans muss {HansTask}...")
 
