5be5e4c99752a58cd9ee8e35a1b953f46db1323d
PSDevChris <christopher.schmid.1993@gmail.com>
2022-01-22 13:49:45 +0100

    Hotfix 2 for Josch Command Slash in the beginning of a path is not allowed

--- Main.py
+++ Main.py
@@ -381,9 +381,10 @@ class Fun(commands.Cog, name="Schabernack"):
         else:
             await ctx.send("Kartoffel API ist leider down T_T")
     
-    @commands.command(name="Josch", aliases=["josch"])
+    @commands.command(name="Josch", aliases=["josch"], brief="Entwickler...")
+    @commands.cooldown(1, 10, commands.BucketType.user)
     async def _blamedevs(self, ctx):
-        await ctx.send(file=discord.File('/memes/josch700#3680/josch.png'))
+        await ctx.send(file=discord.File('memes/josch700#3680/josch.png'))
 
     @commands.command(name="Feiertag", aliases=["feiertag", "holiday", "Holiday"], brief="Zeigt den nächsten Feiertag an")
     @commands.cooldown(1, 10, commands.BucketType.user)
