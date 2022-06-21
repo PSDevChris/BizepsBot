1a55262e329e68a52bbc36c65a3620a938d14953
PSDevChris <christopher.schmid.1993@gmail.com>
2022-06-21 20:26:08 +0200

    Added xkcd cog and modified gitignore

---
+++ cogs/xkcd.py
@@ -0,0 +1,40 @@
+from Main import discord
+from discord.ext import commands
+from Main import _is_banned
+from Main import _get_banned_users
+from Main import datetime
+from Main import aiohttp
+
+
+class xkcd(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.BannedUsers = _get_banned_users()
+
+    async def cog_check(self, ctx):
+        return _is_banned(ctx, self.BannedUsers)
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        pass
+
+    # Commands
+    @commands.command(name="xkcd", aliases=["XKCD", "Xkcd"], brief="Macht Pong")
+    async def _show_xkcd(self, ctx):
+        XkcdURL = "https://xkcd.com/info.0.json"
+        async with aiohttp.ClientSession() as XkcdSession:
+            async with XkcdSession.get(XkcdURL) as RequestToXkcd:
+                if RequestToXkcd.status == 200:
+                    JSONFromXkcd = await RequestToXkcd.json()
+                    XkcdEmbed = discord.Embed(title=f'Aktuelles xkcd Comic: {JSONFromXkcd["safe_title"]}!\r\n', colour=discord.Colour(
+                        0xFFFFFF), description=f'{JSONFromXkcd["alt"]}', timestamp=datetime.utcnow())
+                    XkcdEmbed.set_image(
+                        url=f'{JSONFromXkcd["img"]}')
+                    XkcdEmbed.set_footer(text="Bizeps_Bot")
+                    await ctx.send("", embed=XkcdEmbed)
+
+
+def setup(bot):
+    bot.add_cog(xkcd(bot))
