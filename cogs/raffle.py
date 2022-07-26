d5e6fe7bed8cd3091151394894f4859109fb5f03
PSDevChris <christopher.schmid.1993@gmail.com>
2022-07-26 19:01:21 +0200

    Ported giveaway to Slashcommands Added separate commands for giveaway admin cmds

--- cogs/raffle.py
+++ cogs/raffle.py
@@ -1,9 +1,12 @@
+import discord
+from discord import Option
 from discord.ext import commands
 from Main import _is_banned
 from Main import _read_json
 from Main import _write_json
 from Main import _get_banned_users
-import random
+from Main import logging
+from Main import random
 
 ### Checks ###
 
@@ -28,47 +31,9 @@ class Raffle(commands.Cog, name="Raffle"):
         pass
 
     # Commands
-    @commands.group(name="Raffle", aliases=["raffle"], invoke_without_command=True, brief="Startet ein Raffle oder beendet es.")
-    async def _raffle(self, ctx):
-        RaffleJSON = _read_json('Settings.json')
-        Price =  RaffleJSON['Settings']['Raffle']['Title']
-        if Price != "":
-            await ctx.send(f"Aktuell wird {Price} verlost, tritt einfach mit !raffle join bei!")
-        else:
-            await ctx.send("Aktuell ist keine Verlosung aktiv.")
+    giveaway = discord.SlashCommandGroup("giveaway", "Befehle für Giveaways")
 
-
-    @_raffle.command(name="active", aliases=["Active"], brief="Aktiviert das Raffle")
-    @commands.has_role("Admin")
-    async def _setactive(self, ctx, activeval: bool):
-        activeval = bool(activeval)
-        RaffleJSON = _read_json('Settings.json')
-        RaffleJSON['Settings']['Raffle']['Active'] = activeval
-        _write_json('Settings.json', RaffleJSON)
-        if activeval == True:
-            await ctx.send(f"Das neue Raffle wurde aktiviert! Teilnehmen könnt ihr über !raffle join, verlost wird {RaffleJSON['Settings']['Raffle']['Title']}!")
-        else:
-            RaffleJSON = _read_json('Settings.json')
-            EntryList = list(RaffleJSON['Settings']
-                             ['Raffle']['Entries'].items())
-            if EntryList == []:
-                await ctx.send("Leider hat niemand teilgenommen. Viel Glück beim nächsten Mal!")
-            else:
-                Entry = random.SystemRandom().choice(EntryList)
-                await ctx.send(f"Das Raffle wurde beendet! Gewonnen hat {Entry[0]}! {Entry[1]}")
-            RaffleJSON['Settings']['Raffle']['Entries'] = {}
-            RaffleJSON['Settings']['Raffle']['Title'] = ""
-            _write_json('Settings.json', RaffleJSON)
-
-    @_raffle.command(name="prize", aliases=["Prize"], brief="Setzt den Preis fest")
-    @commands.has_role("Admin")
-    async def _setprize(self, ctx, title: str):
-        RaffleJSON = _read_json('Settings.json')
-        RaffleJSON['Settings']['Raffle']['Title'] = title
-        _write_json('Settings.json', RaffleJSON)
-        await ctx.send(f"Das Raffle für {title} wurde angelegt.")
-
-    @_raffle.command(name="join", aliases=["Join", "enter", "Enter"], brief="Tritt dem Raffle bei")
+    @giveaway.command(name="join", brief="Tritt dem Giveaway bei")
     @commands.check(_raffle_active)
     async def _joinraffle(self, ctx):
         RaffleJSON = _read_json('Settings.json')
@@ -77,10 +42,96 @@ class Raffle(commands.Cog, name="Raffle"):
         }
         if ctx.author.name not in RaffleJSON['Settings']['Raffle']['Entries'].keys():
             RaffleJSON['Settings']['Raffle']['Entries'].update(NewEntry)
-            await ctx.send("Du wurdest zum Raffle hinzugefügt.")
+            await ctx.respond("Du wurdest zum Raffle hinzugefügt.")
         else:
-            await ctx.send("Du bist bereits im Raffle, jeder nur ein Los!")
+            await ctx.respond("Du bist bereits im Raffle, jeder nur ein Los!")
         _write_json('Settings.json', RaffleJSON)
+
+    @giveaway.command(name="show", brief="Zeigt das aktuelle Giveaway")
+    @commands.check(_raffle_active)
+    async def _showraffle(self, ctx):
+        RaffleJSON = _read_json('Settings.json')
+        ctx.respond(
+            f"Aktuell wird {RaffleJSON['Settings']['Raffle']['Title']} verlost!")
+
+    @commands.slash_command(name="set_giveaway", description="Startet ein Giveaway", brief="Startet ein Giveaway")
+    @discord.default_permissions(administrator=True)
+    @commands.has_role("Admin")
+    async def _set_giveaway(self, ctx, prize: Option(str, description="Der Preis", required=True)):
+        """
+        Setzt den Preis für ein Giveaway.
+        """
+        RaffleJSON = _read_json('Settings.json')
+        CurrentPrize = RaffleJSON['Settings']['Raffle']['Title']
+        if CurrentPrize != "":
+            await ctx.respond(f"Aktuell ist {CurrentPrize} noch im Giveaway eingetragen!")
+        else:
+            RaffleJSON['Settings']['Raffle']['Title'] = prize
+            RaffleJSON['Settings']['Raffle']['Active'] = True
+            _write_json('Settings.json', RaffleJSON)
+            await ctx.respond(f"{prize} wurde zum Giveaway hinzugefügt!")
+
+    @commands.slash_command(name="start_giveaway", description="Startet ein Giveaway", brief="Startet ein Giveaway")
+    @discord.default_permissions(administrator=True)
+    @commands.has_role("Admin")
+    async def _start_giveaway(self, ctx):
+        """
+        Startet ein Giveaway.
+        """
+        RaffleJSON = _read_json('Settings.json')
+        CurrentPrize = RaffleJSON['Settings']['Raffle']['Title']
+        CurrentState = RaffleJSON['Settings']['Raffle']['Active']
+        if CurrentPrize != "" and CurrentState:
+            await ctx.respond(f"Das neue Raffle wurde aktiviert! Teilnehmen könnt ihr über !raffle join, verlost wird {RaffleJSON['Settings']['Raffle']['Title']}!")
+        else:
+            await ctx.respond(f"Es fehlt der Preis oder der Status wurde nicht auf aktiv gesetzt! Preis: {CurrentPrize} State: {CurrentState}!")
+
+    @commands.slash_command(name="stop_giveaway", description="Startet ein Giveaway", brief="Startet ein Giveaway")
+    @discord.default_permissions(administrator=True)
+    @commands.has_role("Admin")
+    async def _stop_giveaway(self, ctx):
+        """
+        Beendet ein Giveaway.
+        """
+        RaffleJSON = _read_json('Settings.json')
+        EntryList = list(RaffleJSON['Settings']
+                         ['Raffle']['Entries'].items())
+        if EntryList == []:
+            await ctx.respond("Leider hat niemand teilgenommen. Viel Glück beim nächsten Mal!")
+        else:
+            Entry = random.SystemRandom().choice(EntryList)
+            await ctx.respond(f"Das Raffle wurde beendet! {RaffleJSON['Settings']['Raffle']['Title']} wurde von {Entry[0]}! {Entry[1]} gewonnen!")
+        RaffleJSON['Settings']['Raffle']['Entries'] = {}
+        RaffleJSON['Settings']['Raffle']['Title'] = ""
+        RaffleJSON['Settings']['Raffle']['Active'] = False
+        _write_json('Settings.json', RaffleJSON)
+
+    @_start_giveaway.error
+    async def _showlog_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.respond("Na na, das darf nur der Admin.")
+            logging.warning(f"{ctx.author} wanted to start a giveaway!")
+
+    @_stop_giveaway.error
+    async def _showlog_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.respond("Na na, das darf nur der Admin.")
+            logging.warning(f"{ctx.author} wanted to stop a giveaway!")
+
+    @_set_giveaway.error
+    async def _showlog_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.respond("Na na, das darf nur der Admin.")
+            logging.warning(f"{ctx.author} wanted to set a giveaway!")
+
+    @commands.Cog.listener()
+    async def on_application_command_error(self, ctx: discord.ApplicationContext, error: discord.DiscordException):
+        if isinstance(error, discord.errors.CheckFailure):
+            await ctx.respond("Es ist kein Giveaway aktiv!")
+            logging.info(
+                f"{ctx.author} wanted to join a giveaway, but none are running.")
+        else:
+            logging.error(f"{error}")  # Raise other errors
 
 
 def setup(bot):
