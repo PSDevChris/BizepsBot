263880fbcf5560a4d2f188a27ccda9080175e673
PSDevChris <christopher.schmid.1993@gmail.com>
2022-12-02 09:02:00 +0100

    Rollback to old unload/load behaviour Bug was in the cog itself, not the management cog

--- cogs/management.py
+++ cogs/management.py
@@ -156,8 +156,6 @@ class Management(commands.Cog):
         """
         if changearg == "load":
             self.bot.load_extension(f"cogs.{extension}")
-            if extension == "checkpycordversion":
-                self.bot.reload_extension('checkpycordversion')
             await ctx.respond(f"Extension {extension} wurde geladen und ist jetzt einsatzbereit.")
             logging.info(f"Extension {extension} was loaded.")
         elif changearg == "unload":
