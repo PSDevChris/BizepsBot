a42589ec22e25b283fc57578fb3293bd0e197edb
PSDevChris <christopher.schmid.1993@gmail.com>
2023-01-18 10:02:17 +0100

    Added defers way sooner for memes

--- cogs/memes.py
+++ cogs/memes.py
@@ -106,6 +106,7 @@ class Memes(commands.Cog):
     @commands.slash_command(name="mittwoch", description="Gibt ein Mittwochmeme aus, meine Kerle")
     @commands.cooldown(2, 180, commands.BucketType.user)
     async def _wedmeme(self, ctx, add: discord.Option(str, "Hinzufügen von Memes per add oder collect", choices=["mittwochmeme"], required=False), collect: discord.Option(commands.MessageConverter, "Hinzufügen von Memes per add oder collect", required=False)):
+        await ctx.defer()
         if add:
             LastMessages = await ctx.channel.history(limit=1).flatten()
             if LastMessages[0].author != self.bot.user:
@@ -118,16 +119,16 @@ class Memes(commands.Cog):
                             await meme.save(f"{os.getcwd() + '/memes/'}Mittwoch meine Kerle#/{NumberOfFiles + 1 + index}_{meme.filename}")
                             AllFiles.append(
                                 f"{os.getcwd() + '/memes/'}Mittwoch meine Kerle#/{NumberOfFiles + 1 + index}_{meme.filename}")
-                            await ctx.respond("Mittwoch Memes hinzugefügt.")
+                            await ctx.followup.send("Mittwoch Memes hinzugefügt.")
                             logging.info(
                                 f"{ctx.author} has added a wednesday meme. Name was {meme.filename}")
                         else:
                             logging.error(
                                 f"ERROR: Meme was not under 8mb or not a supported format. Filename was {meme.filename}, size was {meme.size}!")
                 else:
-                    await ctx.respond("Bitte das Meme als Anhang einreichen.")
+                    await ctx.followup.send("Bitte das Meme als Anhang einreichen.")
             else:
-                await ctx.respond("Das Meme stammt von mir, das füge ich nicht nochmal hinzu.")
+                await ctx.followup.send("Das Meme stammt von mir, das füge ich nicht nochmal hinzu.")
         elif collect:
             Message = collect
             if Message.author != self.bot.user:
@@ -140,16 +141,16 @@ class Memes(commands.Cog):
                             await meme.save(f"{os.getcwd() + '/memes/'}Mittwoch meine Kerle#/{NumberOfFiles + 1 + index}_{meme.filename}")
                             logging.info(
                                 f"{ctx.author} has added the wednesday meme {meme.filename}.")
-                            await ctx.respond("Folgendes Mittwoch Meme hinzugefügt:", file=await meme.to_file())
+                            await ctx.followup.send("Folgendes Mittwoch Meme hinzugefügt:", file=await meme.to_file())
                             AllFiles.append(
                                 f"{os.getcwd() + '/memes/'}Mittwoch meine Kerle#/{NumberOfFiles + 1 + index}_{meme.filename}")
                         else:
                             logging.error(
                                 f"ERROR: Meme was not under 8mb or not a supported format. Filename was {meme.filename}, size was {meme.size}!")
                 else:
-                    await ctx.respond("Bitte das Meme als Anhang einreichen.")
+                    await ctx.followup.send("Bitte das Meme als Anhang einreichen.")
             else:
-                await ctx.respond("Das Meme stammt von mir, das füge ich nicht nochmal hinzu.")
+                await ctx.followup.send("Das Meme stammt von mir, das füge ich nicht nochmal hinzu.")
         else:
             if datetime.datetime.now().isoweekday() == 3:
                 WednesdayMemes = list(
@@ -164,11 +165,10 @@ class Memes(commands.Cog):
                 RandomAdjective = random.SystemRandom().choice(MyDudesAdjectives)
                 logging.info(
                     f"{ctx.author} wanted a wednesday meme, chosen adjective was [{RandomAdjective}], chosen meme was [{RandomWedMeme}].")
-                await ctx.defer()
                 await ctx.followup.send(f"Es ist Mittwoch, meine {RandomAdjective} Kerl*innen und \*außen!!!", file=discord.File(f"{RandomWedMeme}"))
                 AllFiles.remove(RandomWedMeme)
             else:
-                await ctx.respond("Es ist noch nicht Mittwoch, mein Kerl.", ephemeral=True)
+                await ctx.followup.send("Es ist noch nicht Mittwoch, mein Kerl.", ephemeral=True)
 
     @_memearchiv.error
     async def _memearchiv_error(self, ctx, error):
