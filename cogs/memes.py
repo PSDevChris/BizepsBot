0bed766ba36e890aa652cfd2dc5d3aff9fda81ad
PSDevChris <christopher.schmid.1993@gmail.com>
2024-06-15 16:58:27 +0200

    Another fix for the new randomizing

--- cogs/memes.py
+++ cogs/memes.py
@@ -150,7 +150,6 @@ class Memes(commands.Cog):
             AuthorOfMeme = RandomMeme.split("/")[-2].split(" ")[0]
             logging.info(f"{ctx.author} wanted a random meme. Chosen was [{RandomMeme}].")
             await ctx.respond(f"Zufalls-Meme! Dieses Meme wurde eingereicht von {AuthorOfMeme}", file=discord.File(f"{RandomMeme}"))
-            AllFiles.remove(RandomMeme)
 
     @commands.slash_command(name="mittwoch", description="Gibt ein Mittwochmeme aus, meine Kerle")
     @commands.cooldown(2, 180, commands.BucketType.user)
@@ -258,7 +257,6 @@ class Memes(commands.Cog):
                 RandomAdjective = random.SystemRandom().choice(MyDudesAdjectives)
                 logging.info(f"{ctx.author} wanted a wednesday meme, chosen adjective was [{RandomAdjective}], chosen meme was [{RandomWedMeme}].")
                 await ctx.respond(f"Es ist Mittwoch, meine {RandomAdjective} Kerl*innen und \*außen!!!", file=discord.File(f"{RandomWedMeme}"))
-                AllFiles.remove(RandomWedMeme)
             else:
                 await ctx.respond("Es ist noch nicht Mittwoch, mein Kerl.")
 
